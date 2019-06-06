f2b0db769fb3d577074a00d4c70ffe94afc54202
nzbenji <benlittle53@gmail.com>
2019-06-06 20:17:33 +1200

    added hangman markup

--- app.js
+++ app.js
@@ -1,38 +1,72 @@
-const app = () => {
-    const HangmanElement = document.querySelector('.hangman');
-    let markup = ``;
+const randomize = () => {
+    return words[Math.floor(Math.random() * words.length)];
+}
 
-    const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    const words = ['javascript','hola', 'banana'];
+const spaceFill = () => {
+    return Array(randomWord.length).fill('_');
+}
 
-    let randomWord,
-        guessingWord,
-        lives,
-        remainingGuesses;
-    
-    const randomize = () => {
-        return words[Math.floor(Math.random() * words.length)];
-    }
+const HangmanElement = document.querySelector('.hangman');
 
-    randomWord = randomize();
-    guessingWord = Array(randomWord.length).fill('_');
+const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const words = ['javascript','hola', 'banana'];
 
-    markup += `
+let randomWord,
+    guessingWord;
+
+let lives = 7;
+let remainingGuesses = [];
+
+randomWord = randomize();
+guessingWord = spaceFill();
+
+let markup = ``;
+
+const home = () => {
+    return markup += `
         <div class="hangman__title">Hangman</div>
         <button class="button start">New Game</button>
         <button class="button instructions">Instructions</button>
     `;
-    
+}
+
+
+HangmanElement.innerHTML = home();
+
+const initPage = () => {
+    let markup = `
+        <p class="hangman__stats">Lives:
+            <span class="hangman__lives">${lives}</span>
+        </p>
+        <h1 class="hangman__title">Hangman></h1>
+        <canvas class="hangman__board height="155px"></canvas>
+        <div class="hangman__word">${guessingWord.join("")}</div>
+        <p class="hangman__instructions">Pick a letter below to guess the whole word</p>
+        <ul class="hangman__letters">
+            ${letters.map(letter => (
+                `<li class="hangman__letter">${letter}</li>`
+            ))}
+        </ul>
+        <button class="button hangman__trigger">Main Menu</button>
+    `
     HangmanElement.innerHTML = markup;
+}
 
-    document.querySelector('.start').addEventListener('click', () => {
-        console.log(randomWord)
-        console.log(guessingWord)
-    });
+HangmanElement.addEventListener('click', event => {
+    
+    if (event.target.matches('.hangman__trigger')) {
+        HangmanElement.innerHTML = markup
+    }
+})
 
-    document.querySelector('.instructions').addEventListener('click', () => {
-        console.log('Instructions')
-    });
-};
 
-app();
+
+document.querySelector('.start').addEventListener('click', () => {
+    console.log(randomWord)
+    console.log(guessingWord)
+    initPage();
+});
+
+document.querySelector('.instructions').addEventListener('click', () => {
+    console.log('Instructions')
+});
