1a60ec753dcfa3e02b6882e36a08255974b17338
nzbenji <benlittle53@gmail.com>
2019-06-09 15:13:40 +1200

    Looping through body parts and displaying on lives

--- modules/Board.js
+++ modules/Board.js
@@ -1,24 +1,29 @@
 const Board = (() => {
     let livesLeft = null;
     let canvas;
-    let context;
+    let ctx;
 
     const init = () => {
         canvas = document.querySelector('.hangman__board');
-        context = canvas.getContext("2d");
-        context.lineWidth = 2;
-        context.strokeStyle = 'white';
+        ctx = canvas.getctx("2d");
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'white';
 
+        hangmanBase();
+    }
+
+    const draw = (startX, startY, endX, endY) => {
+        ctx.moveTo(startX, startY);
+        ctx.lineTo(endX, endY);
+        ctx.stroke();
+    }
+
+    const hangmanBase = () => {
         baseLine();
         verticalLine();
         topLine();
     }
 
-    const draw = (startX, startY, endX, endY) => {
-        context.moveTo(startX, startY);
-        context.lineTo(endX, endY);
-        context.stroke();
-    }
     //Base
     const baseLine = () => draw(0, 150, 150, 150);
     const verticalLine = () => draw(10, 0, 10, 300);
@@ -26,9 +31,9 @@ const Board = (() => {
 
     const rope = () => draw(60, 5, 60, 15);
     const head = () => {
-        context.beginPath();
-        context.arc(60, 25, 10, 0, Math.Pi * 2);
-        context.stroke();
+        ctx.beginPath();
+        ctx.arc(60, 25, 10, 0, Math.PI * 2);
+        ctx.stroke();
     }
     const torso = () => draw(60, 36, 60, 70);
     const rightArm = () => draw(60, 46, 100, 50);
@@ -36,13 +41,15 @@ const Board = (() => {
     const rightLeg = () => draw(60, 70, 100, 100);
     const leftLeg = () => draw(60, 70, 20, 100);
 
-    
+    const parts = [leftLeg, rightLeg, leftArm, rightArm, torso, head, rope]
+
+    const render = () => {
+        parts[livesLeft]();
+    }
 
     const setLives = lives => {
         livesLeft = lives
-        baseLine();
-        verticalLine();
-        topLine();
+        render();
     }
 
     return {
